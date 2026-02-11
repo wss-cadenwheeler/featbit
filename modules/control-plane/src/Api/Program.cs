ab1a71b6231da6962df37e5910c2d1cbbcaea4b2
Caden Wheeler <cawheeler@webstaurantstore.com>
2026-02-11 10:59:10 -0600

    Add infrastructure and application service registrations in Program.cs

--- modules/control-plane/src/Api/Program.cs
+++ modules/control-plane/src/Api/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
