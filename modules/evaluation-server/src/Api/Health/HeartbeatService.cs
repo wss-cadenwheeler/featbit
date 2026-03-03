4e2ac32db99f12c6bf1761576995e694d44eaf13
LANCASTER\jlueck <jlueck@webstaurantstore.com>
2026-03-03 09:34:08 -0500

    Updated heartbeat service for pr comments. Added logging

--- modules/evaluation-server/src/Api/Health/HeartbeatService.cs
+++ modules/evaluation-server/src/Api/Health/HeartbeatService.cs
@@ -5,19 +5,21 @@ namespace Api.Health;
 
 public class HeartbeatService(IMessageProducer messageProducer, ILogger<HeartbeatService> logger) : BackgroundService
 {
-    private readonly ILogger _logger = logger;
     private readonly Guid _podId = Guid.NewGuid();
-    private readonly IMessageProducer _messageProducer = messageProducer;
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (stoppingToken.IsCancellationRequested)
+        logger.LogInformation("HeartbeatService started with PodId: {PodId}", _podId);
+
+        while (!stoppingToken.IsCancellationRequested)
         {
-            await _messageProducer.PublishAsync(Topics.PodHeartbeat, new HealthMessage
+            await messageProducer.PublishAsync(Topics.PodHeartbeat, new HealthMessage
             {
                 PodId = _podId.ToString(),
                 Timestamp = DateTimeOffset.UtcNow
             });
         }
+
+        logger.LogInformation("HeartbeatService stopping with PodId: {PodId}", _podId);
     }
 }
