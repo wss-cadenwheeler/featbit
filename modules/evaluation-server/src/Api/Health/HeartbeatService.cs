a494268704d5469bbca3137116f5f10407232d86
LANCASTER\jlueck <jlueck@webstaurantstore.com>
2026-03-03 14:43:14 -0500

    Made heart beat server configurable

--- modules/evaluation-server/src/Api/Health/HeartbeatService.cs
+++ modules/evaluation-server/src/Api/Health/HeartbeatService.cs
@@ -3,13 +3,17 @@ using Domain.Messages;
 
 namespace Api.Health;
 
-public class HeartbeatService(IMessageProducer messageProducer, ILogger<HeartbeatService> logger) : BackgroundService
+public class HeartbeatService(IMessageProducer messageProducer, ILogger<HeartbeatService> logger, IConfiguration configuration) : BackgroundService
 {
     private readonly Guid _podId = Guid.NewGuid();
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("HeartbeatService started with PodId: {PodId}", _podId);
+
+        var heartbeatInterval = configuration.GetValue<int>("HeartbeatIntervalSeconds");
+
+        var heartbeatTimeSpan = TimeSpan.FromSeconds(heartbeatInterval > 0 ? heartbeatInterval : 60);
 
         while (!stoppingToken.IsCancellationRequested)
         {
@@ -18,6 +22,8 @@ public class HeartbeatService(IMessageProducer messageProducer, ILogger<Heartbea
                 PodId = _podId.ToString(),
                 Timestamp = DateTimeOffset.UtcNow
             });
+
+            await Task.Delay(heartbeatTimeSpan, stoppingToken);
         }
 
         logger.LogInformation("HeartbeatService stopping with PodId: {PodId}", _podId);
